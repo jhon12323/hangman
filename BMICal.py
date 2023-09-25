8b61d412d9d748a56271bc51c7dad901c400e469
jhon12323 <90965488+jhon12323@users.noreply.github.com>
2023-09-25 20:31:00 +0530

    hangman

--- BMICal.py
+++
@@ -1,5 +0,0 @@
-# BMI Calculator
-weight = (int(input("What is your weight?")))
-height = (float(input("What is your height?")))
-BMI = (weight) / (height) ** 2
-print(BMI)
--- Datatypes.py
+++
@@ -1,7 +0,0 @@
-# Type any two no 65 output should be 6+5=11
-
-two_digit_no = input("Type any two digit number?")
-first_no = int(two_digit_no[0])
-second_no = int(two_digit_no[1])
-result = (first_no) + (second_no)
-print(result)
